e339977a5af627e27c45f99755cc26fc1e2e26bd
spsap <spsap@DESKTOP-DEH9IOU>
2022-11-15 15:10:21 +0530

    First Commit

---
+++ Github/src/test/java/pack1/Testclass1.java
@@ -0,0 +1,14 @@
+package pack1;
+
+public class Testclass1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("TC1");
+		System.out.println("TC2");
+		System.out.println("TC3");
+		System.out.println("TC4");
+		System.out.println("TC5");
+	}
+
+}
